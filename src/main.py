a65231431845217125b8e4ae6f8fe2e13aff4fe1
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-01-21 17:27:55 +0900

    Added helper function to hash passwords using bcrypt, and implemented in /register route

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+import bcrypt
 
 
 # User model
@@ -8,6 +9,18 @@ class User(BaseModel):
     email: str
     password: str
     confirm_password: str
+
+
+# Helper function for hashing passwords
+def hash_password(password: str):
+    # Convert plain text password to bytes
+    password_bytes = password.encode("utf-8")
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    # Hash the password using the salt
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    # Convert resulting bytes back to a string for easy storage
+    return hashed_password.decode("utf-8")
 
 
 app = FastAPI()
@@ -22,5 +35,8 @@ def RegisterUser(user: User):
             status_code=status.HTTP_400_BAD_REQUEST, detail="Passwords do not match"
         )
 
+    # Hash password
+    hashed_password = hash_password(user.password)
+
     # Return response object with no passwords for security
-    return {"username": user.username, "email": user.email}
+    return {"username": user.username, "email": user.email, "password": hashed_password}
