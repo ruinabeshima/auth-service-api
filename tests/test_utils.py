f366c07060e6f8750cf8a50da33fc4310fa929ee
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-03 15:00:19 +0900

    Added test for creating and verifying access token

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,10 @@ from src.auth import (
     hash_password,
     verify_password,
 )
+from src.schemas import TokenData
 
-def test_password_hashing():
+
+def test_password_hashing_logic():
     password = "password123"
     hashed_password = hash_password(password)
 
@@ -14,3 +16,12 @@ def test_password_hashing():
     assert password != hashed_password
     assert verify_password("password123", hashed_password) is True
     assert verify_password("password1234", hashed_password) is False
+
+
+def test_access_token_logic():
+    data = TokenData(username="user1")
+    token = create_access_token(data)
+
+    decoded = verify_access_token(token)
+    assert decoded["sub"] == "user1"
+    assert "exp" in decoded
