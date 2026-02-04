ce13a3d329ba3f7e65ac5575d6149c9717897e9f
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-04 13:34:57 +0900

    Added api tests for POST /login route, with flow and bad data

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,8 +7,11 @@ def test_register_flow(client):
         "password": "password123",
         "confirm_password": "password123",
     }
+
     response = client.post("/register", json=test_user)
+
     assert response.status_code == 201
+
     data = response.json()
     assert data["username"] == "testuser123"
     assert data["message"] == "Register successful"
@@ -20,8 +23,11 @@ def test_register_duplicate_username(client):
         "password": "password123",
         "confirm_password": "password123",
     }
+
     client.post("/register", json=test_user)
+
     response = client.post("/register", json=test_user)
+
     assert response.status_code == 400
     data = response.json()
     assert data["detail"] == "Username already exists"
@@ -34,6 +40,61 @@ def test_register_passwords_mismatch(client):
         "confirm_password": "wrong_password123",
     }
     response = client.post("/register", json=test_user)
+
     assert response.status_code == 422
+
     data = response.json()
     assert data["detail"][0]["msg"] == "Value error, Passwords do not match"
+
+
+def test_login_flow(client):
+    test_user = {
+        "username": "testuser123",
+        "password": "password123",
+        "confirm_password": "password123",
+    }
+
+    client.post("/register", json=test_user)
+
+    test_user_login = {"username": "testuser123", "password": "password123"}
+
+    # Uses data= instead of json= because of OAuth2
+    response = client.post("/login", data=test_user_login)
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+
+
+def test_login_password_mismatch(client):
+    test_user = {
+        "username": "testuser123",
+        "password": "password123",
+        "confirm_password": "password123",
+    }
+
+    client.post("/register", json=test_user)
+
+    test_user_login = {"username": "testuser123", "password": "password1234"}
+
+    response = client.post("/login", data=test_user_login)
+
+    assert response.status_code == 401
+    assert response.headers["WWW-Authenticate"] == "Bearer"
+
+    data = response.json()
+    assert data["detail"] == "Invalid username or password"
+
+
+def test_login_nonexistent_user(client):
+    test_user_login = {"username": "testuser123", "password": "password123"}
+
+    response = client.post("/login", data=test_user_login)
+
+    assert response.status_code == 401
+    assert response.headers["WWW-Authenticate"] == "Bearer"
+
+    data = response.json()
+    assert data["detail"] == "Invalid username or password"
