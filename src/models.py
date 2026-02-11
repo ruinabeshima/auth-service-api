75b3929eb719655c8ec484e289430c1f97c7d345
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-11 13:37:10 +0900

    Added new table for Refresh Tokens

--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
 from .database import Base
+from datetime import datetime, timezone
 
 
 class User(Base):
@@ -8,3 +10,22 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+    # One to many relationship: One user can have many refresh tokens
+    refresh_tokens = relationship("RefreshToken", back_populates="user")
+
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, unique=True, index=True)
+    user_id = Column(
+        Integer, ForeignKey("users.id")
+    )  # Foreign key constraint that enforces referential integrity (can't have a refresh token for a non-existent user)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    expires_at = Column(DateTime)
+    # Used to manually invalidate refresh tokens
+    is_revoked = Column(Boolean, default=False)
+
+    # Many to one relationship: Many refresh tokens belong to one user
+    user = relationship("User", back_populates="refresh_tokens")
