3d8e5381a4556621f4766122073ed9171245b6e8
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-18 13:03:19 +0900

    Implemented structured logging in project_services.py

--- src/services/project_service.py
+++ src/services/project_service.py
@@ -2,9 +2,15 @@ from fastapi import HTTPException, status
 from src.models import Project
 from .audit_service import create_audit_log
 from src.schemas import CreateAuditLogRequest
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_project(request, user, db, page, page_size):
+    # Logging
+    logger.info("Get project request", extra={"user_id": user.id})
+
     projects_count = (
         db.query(Project)
         .filter(Project.user_id == user.id, Project.is_deleted == False)
@@ -20,12 +26,11 @@ def get_project(request, user, db, page, page_size):
         .all()
     )
 
-    # Add audit log
-    log_data = CreateAuditLogRequest(
-        user_id=user.id,
-        event_type=f"GET PROJECT SUCCESS: Page {page} of projects shown",
+    # Logging
+    logger.info(
+        f"Get project success - Projects shown",
+        extra={"user_id": user.id, "page": page, "page_size": page_size},
     )
-    create_audit_log(db=db, request=request, log_data=log_data)
 
     return {
         "total": projects_count,
@@ -36,39 +41,49 @@ def get_project(request, user, db, page, page_size):
 
 
 def get_project_by_id(id, request, user, db):
+    # Logging
+    logger.info("Get project by ID request", extra={"user_id": user.id})
+
     project = (
         db.query(Project).filter(Project.id == id, Project.is_deleted == False).first()
     )
 
     if not project:
-        # Add audit log
-        log_data = CreateAuditLogRequest(
-            user_id=user.id,
-            event_type="GET PROJECT BY ID FAILURE: Project not found",
+        # Logging
+        logger.warning(
+            "Get project by id failure - Project does not exist",
+            extra={"user_id": user.id},
         )
-        create_audit_log(db=db, request=request, log_data=log_data)
 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
         )
 
     if project.user_id != user.id and user.role != "admin":
-        # Add audit log
-        log_data = CreateAuditLogRequest(
-            user_id=user.id,
-            event_type="GET PROJECT BY ID FAILURE: Not authorised to view project",
+        # Logging
+        logger.warning(
+            "Get project by id failure - not authorized to view project",
+            extra={"user_id": user.id},
         )
-        create_audit_log(db=db, request=request, log_data=log_data)
 
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="You are not authorized to view this project",
         )
 
+    # Logging
+    logger.info(
+        "Get projecy by id success - project shown",
+        extra={"user_id": user.id, "project_id": project.id},
+    )
+
     return project
 
 
 def add_project(request, project_data, user, db):
+    # Logging
+    logger.info("Add project request", extra={"user_id": user.id})
+
     new_project = Project(
         name=project_data.name, description=project_data.description, user_id=user.id
     )
@@ -77,7 +92,13 @@ def add_project(request, project_data, user, db):
     db.commit()
     db.refresh(new_project)
 
-    # Add audit log
+    # Logging
+    logger.info(
+        "Get project success - project created",
+        extra={"user_id": user.id, "project_id": new_project.id},
+    )
+
+    # Audit log
     log_data = CreateAuditLogRequest(
         user_id=user.id,
         event_type="CREATE PROJECT SUCCESS: Project created",
@@ -88,11 +109,19 @@ def add_project(request, project_data, user, db):
 
 
 def update_project(id, request, project_update, user, db):
+    # Logging
+    logger.info("Update project request", extra={"user_id": user.id})
+
     # Get already existing project
     project = db.query(Project).filter(Project.id == id).first()
 
     if not project:
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Update project failure - project not found", extra={"user_id": user.id}
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="UPDATE PROJECT FAILURE:  Project not found",
@@ -105,7 +134,12 @@ def update_project(id, request, project_update, user, db):
         )
 
     if project.user_id != user.id and user.role != "admin":  # type: ignore
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Update project failure - unauthorized", extra={"user_id": user.id}
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="UPDATE PROJECT FAILURE: Not allowed to update project",
@@ -118,7 +152,12 @@ def update_project(id, request, project_update, user, db):
         )
 
     if project.is_deleted:  # type: ignore
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Update project failure - project deleted", extra={"user_id": user.id}
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="UPDATE PROJECT FAILURE:  Project has been deleted",
@@ -138,7 +177,13 @@ def update_project(id, request, project_update, user, db):
     db.commit()
     db.refresh(project)
 
-    # Add audit log
+    # Logging
+    logger.info(
+        "Update project success - project updated",
+        extra={"user_id": user.id, "project_id": project.id},
+    )
+
+    # Audit log
     log_data = CreateAuditLogRequest(
         user_id=user.id,
         event_type="UPDATE PROJECT SUCCESS: Project has been updated",
@@ -149,11 +194,19 @@ def update_project(id, request, project_update, user, db):
 
 
 def delete_project(id, request, user, db):
+    # Logging
+    logger.info("Delete project request", extra={"user_id": user.id})
+
     # Get already existing project
     project = db.query(Project).filter(Project.id == id).first()
 
     if not project:
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Delete project failure - Project not found", extra={"user_id": user.id}
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="DELETE PROJECT FAILURE:  Project not found",
@@ -166,7 +219,12 @@ def delete_project(id, request, user, db):
         )
 
     if project.user_id != user.id and user.role != "admin":  # type: ignore
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Delete project failure - unauthorized", extra={"user_id": user.id}
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="DELETE PROJECT FAILURE: Not allowed to delete project",
@@ -179,7 +237,13 @@ def delete_project(id, request, user, db):
         )
 
     if project.is_deleted:  # type: ignore
-        # Add audit log
+        # Logging
+        logger.warning(
+            "Delete project failure - project deleted already",
+            extra={"user_id": user.id},
+        )
+
+        # Audit log
         log_data = CreateAuditLogRequest(
             user_id=user.id,
             event_type="DELETE PROJECT FAILURE:  Project has been deleted already",
@@ -195,7 +259,13 @@ def delete_project(id, request, user, db):
     db.commit()
     db.refresh(project)
 
-    # Add audit log
+    # Logging
+    logger.info(
+        "Project deleted successfully",
+        extra={"user_id": user.id, "project_id": project.id},
+    )
+
+    # Audit log
     log_data = CreateAuditLogRequest(
         user_id=user.id,
         event_type="DELETE PROJECT SUCCESS: Project has been deleted successfully",
